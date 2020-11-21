913df72a4426962833e9f2a14b51aa0e86d6b597
Ota Hauptmann <ota.hauptmann@gmail.com>
2020-11-21 14:13:40 +0100

    Implement testing app

--- src/test/scala/io/github/otah/hap/test/TestRunner.scala
+++ src/test/scala/io/github/otah/hap/test/TestRunner.scala
@@ -1,31 +1,82 @@
 package io.github.otah.hap.test
 
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.{AtomicBoolean, AtomicInteger}
+
 import com.github.otah.hap.api._
 import com.github.otah.hap.api.characteristics.PowerStateCharacteristic
+import com.github.otah.hap.api.information.Revision
 import com.github.otah.hap.api.server._
-import com.github.otah.hap.api.services.SwitchService
+import com.github.otah.hap.api.services.experimental._
+
+import scala.concurrent.Future
 
 object TestRunner extends App {
 
-  val switch1 = new HomeKitAccessory with SingleServiceAccessory with SwitchService {
+  val switch1 = new HomeKitAccessory with SingleServiceAccessory with SwitchService with SequenceInstanceIds {
 
-    override def info: HomeKitInfo = ???
-    override def services: Services = ???
+    override val info: HomeKitInfo = new HomeKitInfo {
+      override def identification: () => Unit = () => println("Hello switch!")
+      override def label: String = "Switch1"
+      override def serialNumber: String = "none"
+      override def model: String = "testswitch1"
+      override def manufacturer: String = "Otah"
+      override def firmwareRevision: Revision = Revision("0.1")
+      override def hardwareRevision: Option[Revision] = None
+    }
 
-    override def powerState: Required[PowerStateCharacteristic] = 20 <=> new PowerStateCharacteristic {
+    override val powerState: PowerStateCharacteristic = new PowerStateCharacteristic {
 
-      override def reader: Some[Reader] = ???
+      private val notifierCounter = new AtomicInteger(1)
+      private val state = new AtomicBoolean(false)
+      private val notifiers = new ConcurrentHashMap[Int, Boolean => Future[Unit]]()
 
-      override def writer: Some[Writer] = ???
+      override def reader: Some[Reader] = Reader(Future.successful(state.get()))
 
-      override def notifier: Some[Notifier] = ???
+      override def writer: Some[Writer] = Writer { newValue =>
+        state.set(newValue)
+        println(s"Set new value: $newValue")
+        notifiers.values().forEach(n => Future.successful(n(newValue)))
+        Future.unit
+      }
+
+      override def notifier: Some[Notifier] = Some { (callback: Boolean => Future[Unit]) =>
+        new Subscription {
+          private val id = notifierCounter.getAndIncrement()
+          notifiers.put(id, callback)
+
+          override def unsubscribe(): Unit = notifiers.remove(id)
+        }
+      }
     }
   }
 
-  val bridge = ???
-  val auth = ???
+  val bridge = new HomeKitBridge {
+    override def info: HomeKitInfo = new HomeKitInfo {
+      override def identification: () => Unit = () => println("Hello bridge!")
+      override def label: String = "Bridge1"
+      override def serialNumber: String = "none"
+      override def model: String = "testbridge1"
+      override def manufacturer: String = "Otah"
+      override def firmwareRevision: Revision = Revision("0.1")
+      override def hardwareRevision: Option[Revision] = None
+    }
+    override def accessories: Seq[Identified[HomeKitAccessory]] = Seq(
+      3 <=> switch1,
+    )
+  }
 
-  val serverDefinition = HomeKitServer(1234)(HomeKitRoot.bridge(bridge, auth))
+  import io.github.hapjava.server.impl.{HomekitUtils => utils}
+  val pin = utils.generatePin()
+
+  println(s"PIN: $pin")
+
+  val auth = HomeKitAuthentication(
+    AuthInfoStorage(initialUserKeys = Map.empty, onChange = _ => ()),
+    AuthSecurityInfo(utils.generateMac(), utils.generateSalt(), utils.generateKey().toSeq, pin),
+  )
+
+  val serverDefinition = HomeKitServer(port = 1234, host = Some("10.11.0.156"))(HomeKitRoot.bridge(bridge, auth))
 
   val server = new io.github.hapjava.server.impl.HomekitServer(serverDefinition)
   server.start()
